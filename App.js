018761298deb6ab2695b4d1dac06368f44e2abd8
RealMarkSussex <marksussex6@gmail.com>
2020-12-12 19:19:33 +0000

    awesome app

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, ScrollView, TextInput } from 'react-native';
 
 export default function App() {
+  const [wikiContent, setWikiContent] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ScrollView>
+      <TextInput
+        placeholder="type in me!"
+        onChangeText={(text) => handleTextChange(text, setWikiContent)}
+        style={ {textAlign: "center", padding: 10, fontSize: 42} }
+      />
+      <Text>{wikiContent}</Text>
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const handleTextChange = async (text, setWikiContent) => {
+  const searchUrl =
+    "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=";
+  const contextUrl =
+    "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=";
+  let searchResponse = await fetch(searchUrl + text);
+  let search = await searchResponse.json();
+  let title1 = search[1][1];
+
+  let contentResponse = await fetch(contextUrl + title1);
+  let content = await contentResponse.json();
+  let page = content.query.pages;
+  let pageId = Object.keys(page)[0];
+
+  setWikiContent(page[pageId].revisions[0]["*"]);
+};
+
+
